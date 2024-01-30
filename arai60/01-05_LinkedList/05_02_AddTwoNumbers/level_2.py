2e98bec2a93a27ceff4d2173ea6407a90aa81c18
shining-ai <tderkkwii+social@gmail.com>
2024-01-30 21:32:01 +0000

    変数名の修正と改行の削除

--- arai60/01-05_LinkedList/05_02_AddTwoNumbers/level_2.py
+++ arai60/01-05_LinkedList/05_02_AddTwoNumbers/level_2.py
@@ -7,26 +7,21 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        ans_head = ListNode(0)
-        ans = ans_head
+        sentinel = ListNode(0)
+        current = sentinel
         carry = 0
-
         while l1 or l2 or carry:
             v1 = 0
             if l1:
                 v1 = l1.val
                 l1 = l1.next
-
             v2 = 0
             if l2:
                 v2 = l2.val
                 l2 = l2.next
-
-            l_sum = v1 + v2 + carry
-            carry = l_sum // 10
-            new_val = l_sum % 10
-
-            ans.next = ListNode(new_val)
-            ans = ans.next
-
-        return ans_head.next
+            current_val = v1 + v2 + carry
+            carry = current_val // 10
+            current_digit = current_val % 10
+            current.next = ListNode(current_digit)
+            current = current.next
+        return sentinel.next
