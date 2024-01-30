6e238b06db6e88cd2beff972f82f28365bb87722
shining-ai <tderkkwii+social@gmail.com>
2024-01-30 20:06:55 +0000

    改行を削除

--- arai60/01-05_LinkedList/02_142_LinkedListCycleII/level_2.py
+++ arai60/01-05_LinkedList/02_142_LinkedListCycleII/level_2.py
@@ -8,12 +8,9 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         visited = set()
-
         while head:
             if head in visited:
                 return head
-
             visited.add(head)
             head = head.next
-
         return None
