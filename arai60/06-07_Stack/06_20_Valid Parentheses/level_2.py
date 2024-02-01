ccef231006fae3b7efea303d95b7d93ea495eb41
shining-ai <tderkkwii+social@gmail.com>
2024-02-01 22:53:20 +0000

    変数名を修正

--- arai60/06-07_Stack/06_20_Valid Parentheses/level_2.py
+++ arai60/06-07_Stack/06_20_Valid Parentheses/level_2.py
@@ -2,15 +2,13 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         bracket_pairs = {"(": ")", "[": "]", "{": "}"}
-        for i_char in s:
-            if i_char in bracket_pairs:
-                stack.append(i_char)
+        for bracket in s:
+            if bracket in bracket_pairs:
+                stack.append(bracket)
                 continue
             if not stack:
                 return False
-            if i_char != bracket_pairs[stack[-1]]:
+            if bracket != bracket_pairs[stack[-1]]:
                 return False
             stack.pop()
-        if stack:
-            return False
-        return True
+        return not stack
