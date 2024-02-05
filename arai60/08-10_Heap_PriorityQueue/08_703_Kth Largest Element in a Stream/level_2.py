4572662189c7a825cc3a26afa126ad551864d27b
shining-ai <tderkkwii+social@gmail.com>
2024-02-05 18:28:40 +0000

    init内でadd関数を使用する形に修正

--- arai60/08-10_Heap_PriorityQueue/08_703_Kth Largest Element in a Stream/level_2.py
+++ arai60/08-10_Heap_PriorityQueue/08_703_Kth Largest Element in a Stream/level_2.py
@@ -4,13 +4,12 @@ import heapq
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.nums = nums
-        heapq.heapify(self.nums)
-        while len(self.nums) > k:
-            heapq.heappop(self.nums)
+        self.min_heap = []
+        for num in nums:
+            self.add(num)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.nums, val)
-        if len(self.nums) > self.k:
-            heapq.heappop(self.nums)
-        return self.nums[0]
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
