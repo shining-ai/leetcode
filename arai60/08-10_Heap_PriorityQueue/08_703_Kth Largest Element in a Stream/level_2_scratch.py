b9c4b9a2cccaed163741b0d56d053c1116ba9806
shining-ai <tderkkwii+social@gmail.com>
2024-02-06 15:33:05 +0000

    例示された流れに沿うように修正

--- arai60/08-10_Heap_PriorityQueue/08_703_Kth Largest Element in a Stream/level_2_scratch.py
+++ arai60/08-10_Heap_PriorityQueue/08_703_Kth Largest Element in a Stream/level_2_scratch.py
@@ -1,58 +1,73 @@
-def _swap_child(nums: List[int], parent_pos: int):
-    end_pos = len(nums)
-    child_pos = 2 * parent_pos + 1  # left child
-    if child_pos >= end_pos:
-        return None
-    right_child_pos = child_pos + 1
-    if right_child_pos < end_pos and nums[right_child_pos] < nums[child_pos]:
-        child_pos = right_child_pos
-    if nums[child_pos] < nums[parent_pos]:
-        nums[parent_pos], nums[child_pos] = nums[child_pos], nums[parent_pos]
-        return child_pos
-    return None
+class PriorityQueue:
+    def __init__(self, nums: List[int]):
+        self.heap = []
+        for num in nums:
+            self.push(num)
 
+    def __parent_index(self, index: int) -> int:
+        return (index - 1) // 2
 
-def make_priority_queue(nums: List[int]):
-    end_pos = len(nums)
-    for parent_pos in reversed(range(end_pos // 2)):
-        swap_pos = _swap_child(nums, parent_pos)
-        while swap_pos is not None:
-            swap_pos = _swap_child(nums, swap_pos)
-    return nums
+    def __left_child_index(self, index: int) -> int:
+        return 2 * index + 1
 
+    def __right_child_index(self, index: int) -> int:
+        return 2 * index + 2
 
-def push_priority_queue(nums: List[int], val: int):
-    nums.append(val)
-    pos = len(nums) - 1
-    while pos > 0:
-        parent_pos = (pos - 1) // 2
-        if nums[pos] < nums[parent_pos]:
-            nums[parent_pos], nums[pos] = nums[pos], nums[parent_pos]
-            pos = parent_pos
-            continue
-        break
-    return nums
+    def __has_child(self, index: int) -> bool:
+        return self.__left_child_index(index) < len(self.heap)
 
+    def __has_right_child(self, index: int) -> bool:
+        return self.__right_child_index(index) < len(self.heap)
 
-def pop_priority_queue(nums: List[int]):
-    nums[0] = nums[-1]
-    nums.pop()
-    pos = 0
-    child_pos = 2 * pos + 1
-    while pos is not None:
-        pos = _swap_child(nums, pos)
-    return nums
+    def __smaller_child(self, index: int) -> int:
+        smaller_chiled_index = self.__left_child_index(index)
+        if (
+            self.__has_right_child(index)
+            and self.heap[self.__right_child_index(index)]
+            < self.heap[smaller_chiled_index]
+        ):
+            smaller_chiled_index = self.__right_child_index(index)
+        return smaller_chiled_index
+
+    def push(self, val: int):
+        added_index = len(self.heap)
+        self.heap.append(val)
+        while added_index > 0:
+            parent_index = self.__parent_index(added_index)
+            if self.heap[added_index] > self.heap[parent_index]:
+                break
+            self.heap[added_index], self.heap[parent_index] = (
+                self.heap[parent_index],
+                self.heap[added_index],
+            )
+            added_index = parent_index
+
+    def pop(self) -> int:
+        smallest = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        added_index = 0
+        while self.__has_child(added_index):
+            smaller_child_index = self.__smaller_child(added_index)
+            if self.heap[added_index] < self.heap[smaller_child_index]:
+                break
+            self.heap[added_index], self.heap[smaller_child_index] = (
+                self.heap[smaller_child_index],
+                self.heap[added_index],
+            )
+            added_index = smaller_child_index
+        return smallest
 
 
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.nums = make_priority_queue(nums)
-        while len(self.nums) > k:
-            self.nums = pop_priority_queue(self.nums)
+        self.priority_queue = PriorityQueue([])
+        for num in nums:
+            self.add(num)
 
     def add(self, val: int) -> int:
-        self.nums = push_priority_queue(self.nums, val)
-        if len(self.nums) > self.k:
-            self.nums = pop_priority_queue(self.nums)
-        return self.nums[0]
+        self.priority_queue.push(val)
+        if len(self.priority_queue.heap) > self.k:
+            self.priority_queue.pop()
+        return self.priority_queue.heap[0]
