81ef50405edaaf040be8dbc35fb1cdb5211ae6b2
shining-ai <tderkkwii+social@gmail.com>
2024-02-27 14:29:03 +0000

    コメントの内容を反映

--- arai60/11-16_HashMap/16_560_Subarray Sum Equals K/level_2.py
+++ arai60/11-16_HashMap/16_560_Subarray Sum Equals K/level_2.py
@@ -2,14 +2,14 @@
 # 1段目から変更なし
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        subarray_num = 0
+        count = 0
         for i in range(len(nums)):
-            sum = 0
-            for num in nums[i:]:
-                sum += num
-                if sum == k:
-                    subarray_num += 1
-        return subarray_num
+            sum_val = 0
+            for j in range(i, len(nums)):
+                sum_val += nums[j]
+                if sum_val == k:
+                    count += 1
+        return count
 
 
 # 累積和を使って全探索する
@@ -19,12 +19,12 @@ class Solution:
         num_sums = [0]
         for i, num in enumerate(nums):
             num_sums.append(num_sums[i] + num)
-        subarray_num = 0
+        count = 0
         for i, start in enumerate(num_sums):
             for end in num_sums[i + 1 :]:
                 if end - start == k:
-                    subarray_num += 1
-        return subarray_num
+                    count += 1
+        return count
 
 
 # 累積和をハッシュマップに保存して探索する
@@ -32,12 +32,11 @@ class Solution:
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         sum_freq = defaultdict(int)
-        sum_freq[0] = 1
-        subarray_num = 0
-        sum = 0
+        count = 0
+        sum_val = 0
         for num in nums:
-            sum += num
-            target = sum - k
-            subarray_num += sum_freq[target]
-            sum_freq[sum] += 1
-        return subarray_num
+            sum_freq[sum_val] += 1
+            sum_val += num
+            target = sum_val - k
+            count += sum_freq[target]
+        return count
