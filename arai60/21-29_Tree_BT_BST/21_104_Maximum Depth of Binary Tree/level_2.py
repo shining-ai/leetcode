8da13b0013e57c3ba00cc2e31814faa630ad6922
shining-ai <tderkkwii+social@gmail.com>
2024-03-10 08:23:38 +0000

    コメントの内容を反映

--- arai60/21-29_Tree_BT_BST/21_104_Maximum Depth of Binary Tree/level_2.py
+++ arai60/21-29_Tree_BT_BST/21_104_Maximum Depth of Binary Tree/level_2.py
@@ -2,16 +2,12 @@
 # 特に変更なし
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
-        def search_max_depth(node):
-            if not node:
-                return 0
-            left_depth = search_max_depth(node.left) + 1
-            right_depth = search_max_depth(node.right) + 1
-            max_depth = max(left_depth, right_depth)
-            return max_depth
-
-        return search_max_depth(root)
+        if not root:
+            return 0
+        left_depth = self.maxDepth(root.left) + 1
+        right_depth = self.maxDepth(root.right) + 1
+        max_depth = max(left_depth, right_depth)
+        return max_depth
 
 
 # BFS
@@ -21,14 +17,13 @@ class Solution:
         if not root:
             return 0
         node_depth_queue = deque([(root, 1)])
-        max_depth = 0
         while node_depth_queue:
             node, depth = node_depth_queue.popleft()
             if node.left:
                 node_depth_queue.append((node.left, depth + 1))
             if node.right:
                 node_depth_queue.append((node.right, depth + 1))
-            max_depth = max(max_depth, depth)
+        max_depth = depth
         return max_depth
 
 
