517dc578ba2c36f7ed0b468af3109ef6498f7cbb
shining-ai <tderkkwii+social@gmail.com>
2024-03-13 12:18:14 +0000

    DFSのhelper関数を削除

--- arai60/21-29_Tree_BT_BST/23_617_Merge Two Binary Trees/level_4.py
+++ arai60/21-29_Tree_BT_BST/23_617_Merge Two Binary Trees/level_4.py
@@ -1,22 +1,18 @@
 # DFS(非破壊)
 # レビューコメントを反映
+# helper関数をなくした
 class Solution:
     def mergeTrees(
         self, root1: Optional[TreeNode], root2: Optional[TreeNode]
     ) -> Optional[TreeNode]:
-        def helper_merge_trees(node1, node2):
-            if not node2:
-                return node1
-            if not node1:
-                return deepcopy(node2)
-            node1.val += node2.val
-            node1.left = helper_merge_trees(node1.left, node2.left)
-            node1.right = helper_merge_trees(node1.right, node2.right)
-            return node1
-
-        root1_clone = deepcopy(root1)
-        return helper_merge_trees(root1_clone, root2)
-
+            if not root2:
+                return deepcopy(root1)
+            if not root1:
+                return deepcopy(root2)
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
+            return node
 
 # BFS(非破壊)
 # レビューコメントを反映
