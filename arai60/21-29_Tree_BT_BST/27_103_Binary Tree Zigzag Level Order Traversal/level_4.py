ff3a74f7a54db616ade547f7f48d57687ccb66ab
shining-ai <tderkkwii+social@gmail.com>
2024-03-18 18:44:19 +0000

    コメントの内容を反映

--- arai60/21-29_Tree_BT_BST/27_103_Binary Tree Zigzag Level Order Traversal/level_4.py
+++ arai60/21-29_Tree_BT_BST/27_103_Binary Tree Zigzag Level Order Traversal/level_4.py
@@ -1,24 +1,52 @@
 # BFS
 # 変数名を修正
+# queueの中身の個数でlevelを判定していた部分を修正(current_queueとnext_queueを使うように変更)
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        node_queue = deque([root])
+        current_queue = deque([root])
         zigzag_order = []
         level = 0
-        while node_queue:
-            num_node_in_level = len(node_queue)
+        while current_queue:
             values_in_level = []
-            for _ in range(num_node_in_level):
-                node = node_queue.popleft()
+            next_queue = deque()
+            while current_queue:
+                node = current_queue.popleft()
                 values_in_level.append(node.val)
                 if node.left:
-                    node_queue.append(node.left)
+                    next_queue.append(node.left)
                 if node.right:
-                    node_queue.append(node.right)
+                    next_queue.append(node.right)
             if level % 2:
                 values_in_level.reverse()
             zigzag_order.append(values_in_level)
             level += 1
+            current_queue = next_queue
+        return zigzag_order
+
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        zigzag_order = []
+
+        def append_zigzag_order(node, level):
+            if level % 2 == 0:
+                zigzag_order[level].append(node.val)
+            else:
+                zigzag_order[level].appendleft(node.val)
+
+        def build_zigzag_order(node, level):
+            while len(zigzag_order) - 1 < level:
+                zigzag_order.append(deque())
+            append_zigzag_order(node, level)
+            if node.left:
+                build_zigzag_order(node.left, level + 1)
+            if node.right:
+                build_zigzag_order(node.right, level + 1)
+
+        build_zigzag_order(root, 0)
+        zigzag_order = list(map(list, zigzag_order))
         return zigzag_order
