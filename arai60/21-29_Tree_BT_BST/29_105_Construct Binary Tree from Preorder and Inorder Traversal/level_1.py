fecbaaa8893f7bf5656e82f5e8d07a8f86c6da74
shining-ai <tderkkwii+social@gmail.com>
2024-03-20 12:38:13 +0000

    splitの誤植を修正

--- arai60/21-29_Tree_BT_BST/29_105_Construct Binary Tree from Preorder and Inorder Traversal/level_1.py
+++ arai60/21-29_Tree_BT_BST/29_105_Construct Binary Tree from Preorder and Inorder Traversal/level_1.py
@@ -4,18 +4,18 @@ class Solution:
     ) -> Optional[TreeNode]:
         current_preorder_index = 0
 
-        def array_to_tree(splied_inorder):
+        def array_to_tree(split_inorder):
             nonlocal current_preorder_index
             if current_preorder_index >= len(preorder):
                 return None
             val = preorder[current_preorder_index]
-            if not val in splied_inorder:
+            if not val in split_inorder:
                 return None
             current_preorder_index += 1
             node = TreeNode(val)
-            inorder_index = splied_inorder.index(val)
-            node.left = array_to_tree(splied_inorder[:inorder_index])
-            node.right = array_to_tree(splied_inorder[inorder_index + 1 :])
+            inorder_index = split_inorder.index(val)
+            node.left = array_to_tree(split_inorder[:inorder_index])
+            node.right = array_to_tree(split_inorder[inorder_index + 1 :])
             return node
 
         return array_to_tree(inorder)
