e9057d2420e23f463c9415b0a1c64490f1ca6a58
shining-ai <tderkkwii+social@gmail.com>
2024-03-29 10:19:24 +0000

    余計な改行を削除

--- arai60/30-40_Dynamic Programming/35_198_House Robber/level_1.py
+++ arai60/30-40_Dynamic Programming/35_198_House Robber/level_1.py
@@ -7,11 +7,9 @@ class Solution:
         max_robbed_amount = [0] * len(nums)
         max_robbed_amount[0] = nums[0]
         max_robbed_amount[1] = max(nums[0], nums[1])
-
         for i in range(2, len(nums)):
             max_robbed_amount[i] = max(
                 max_robbed_amount[i - 2] + nums[i],
                 max_robbed_amount[i - 1],
             )
-
         return max_robbed_amount[-1]
