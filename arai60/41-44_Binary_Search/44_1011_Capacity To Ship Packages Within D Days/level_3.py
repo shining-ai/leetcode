fe84025707d2cf37c4e94534397a671d344247cc
shining-ai <tderkkwii+social@gmail.com>
2024-04-06 08:48:47 +0000

    【Arai60】44問目 1011. Capacity To Ship Packages Within D Days

--- arai60/41-44_Binary_Search/44_1011_Capacity To Ship Packages Within D Days/level_3.py
+++ arai60/41-44_Binary_Search/44_1011_Capacity To Ship Packages Within D Days/level_3.py
@@ -1,13 +1,9 @@
-# 解答と同様の実装
-# 運べるかどうかを試して、capacityを2分探索する
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        def can_shipp(capacity):
+        def can_ship(capacity):
             needed_day = 1
             current_loaded = 0
             for weight in weights:
-                if capacity < weight:
-                    return False
                 current_loaded += weight
                 if current_loaded <= capacity:
                     continue
@@ -19,10 +15,9 @@ class Solution:
 
         left = max(weights)
         right = sum(weights)
-
         while left < right:
             middle = (left + right) // 2
-            if can_shipp(middle):
+            if can_ship(middle):
                 right = middle
             else:
                 left = middle + 1
