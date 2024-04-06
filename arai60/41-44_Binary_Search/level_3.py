47cb79d25a601a752ef1e7f047857bb235bc528c
shining-ai <tderkkwii+social@gmail.com>
2024-04-06 06:10:19 +0000

    【Arai60】43問目 33. Search in Rotated Sorted Array

---
+++ arai60/41-44_Binary_Search/level_3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def find_pivot_index():
+            left = 0
+            right = len(nums) - 1
+            while left < right:
+                middle = (left + right) // 2
+                if nums[middle] < nums[right]:
+                    right = middle
+                else:
+                    left = middle + 1
+            return left
+
+        pivot_index = find_pivot_index()
+        if nums[pivot_index] <= target <= nums[-1]:
+            left = pivot_index
+            right = len(nums)
+        else:
+            left = 0
+            right = pivot_index
+        target_index = bisect_left(nums, target, left, right)
+        if nums[target_index] == target:
+            return target_index
+        return -1
