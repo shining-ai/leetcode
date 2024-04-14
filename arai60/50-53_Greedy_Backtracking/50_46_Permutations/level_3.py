40d8fd02ed60a883ba89e837a78567f913f2ed2f
shining-ai <tderkkwii+social@gmail.com>
2024-04-14 03:45:19 +0000

    【Arai60】50問目 46. Permutations

---
+++ arai60/50-53_Greedy_Backtracking/50_46_Permutations/level_3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def make_permutations(current):
+            if len(current) == len(nums):
+                permutations.append(current[:])
+                return
+            for num in nums:
+                if num in current:
+                    continue
+                current.append(num)
+                make_permutations(current)
+                current.pop()
+
+        permutations = []
+        make_permutations([])
+        return permutations
