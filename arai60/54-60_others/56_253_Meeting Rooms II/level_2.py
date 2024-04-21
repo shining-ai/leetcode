e8fb3b191b987dffafc1f97e7b06d896eeeb7b14
shining-ai <tderkkwii+social@gmail.com>
2024-04-21 03:25:12 +0000

    sortを関数に切り出し

--- arai60/54-60_others/56_253_Meeting Rooms II/level_2.py
+++ arai60/54-60_others/56_253_Meeting Rooms II/level_2.py
@@ -2,15 +2,19 @@
 # 開始時刻になったら会議を増やし、終了時刻になったら会議数を減らせば、開かれている会議数が分かる
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        sorted_intervals = []
-        for interval in intervals:
-            sorted_intervals.append((interval[0], "start"))
-            sorted_intervals.append((interval[1], "end"))
-        sorted_intervals.sort()
+
+        def sort_events(intervals):
+            sorted_events = []
+            for interval in intervals:
+                sorted_events.append((interval[0], "start"))
+                sorted_events.append((interval[1], "end"))
+            return sorted(sorted_events)
+
+        sorted_events = sort_events(intervals)
         max_rooms = 0
         rooms = 0
-        for _, time_type in sorted_intervals:
-            if time_type == "start":
+        for _, event_type in sorted_events:
+            if event_type == "start":
                 rooms += 1
                 max_rooms = max(max_rooms, rooms)
             else:
