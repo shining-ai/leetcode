e642cb30db3fa07b906dc606f8b5b4863d106921
shining-ai <tderkkwii+social@gmail.com>
2024-04-25 14:49:48 +0000

    不要な条件を削除

---
+++ arai60/50-53_Greedy_Backtracking/51_78_Subsets/level_5.py
@@ -0,0 +1,14 @@
+# Backtracking
+# subsetの長さの指定を不要にした。
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def make_subset(begin, current):
+            possible_subsets.append(current[:])
+            for i in range(begin, len(nums)):
+                current.append(nums[i])
+                make_subset(i + 1, current)
+                current.pop()
+
+        possible_subsets = []
+        make_subset(0, [])
+        return possible_subsets
