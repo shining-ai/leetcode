1a52fe4176bd67da63e9d447b70bbbe5b6673084
shining-ai <tderkkwii+social@gmail.com>
2024-04-28 13:27:42 +0000

    ローリングハッシュの解法を追加

--- arai60/30-40_Dynamic Programming/39_139_Word Break/level_5.py
+++ arai60/30-40_Dynamic Programming/39_139_Word Break/level_5.py
@@ -42,3 +42,36 @@ class Solution:
         pattern.pop()
         pattern.append(")*")
         return re.match("".join(pattern), s).span() == (0, len(s))
+
+
+# ローリングハッシュ
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        base = 256
+        prime = 109  # 任意の素数
+
+        @cache
+        def make_hash(word: str) -> int:
+            hash_value = 0
+            for i in range(len(word)):
+                hash_value += (
+                    ord(word[i]) * base ** (len(word) - i - 1)
+                ) % prime
+            hash_value %= prime
+            return hash_value
+
+        hash_list = []
+        for word in wordDict:
+            hash_value = make_hash(word)
+            hash_list.append((len(word), hash_value))
+
+        word_end = [False] * len(s)
+        for start in range(len(s)):
+            if start != 0 and not word_end[start - 1]:
+                continue
+            for length, hash_value in hash_list:
+                if start + length > len(s):
+                    continue
+                if hash_value == make_hash(s[start : start + length]):
+                    word_end[start + length - 1] = True
+        return word_end[-1]
