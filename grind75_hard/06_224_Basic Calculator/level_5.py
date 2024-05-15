4d67ead979e5afec3aaafbfe4944b55401ae2459
shining-ai <tderkkwii+social@gmail.com>
2024-05-15 13:15:28 +0000

    Revert "スペースを最初に取り除く"
    This reverts commit 2c68e3809c964e3577fa97b335974e5b549d761f.

--- grind75_hard/06_224_Basic Calculator/level_5.py
+++ grind75_hard/06_224_Basic Calculator/level_5.py
@@ -3,12 +3,18 @@ class Solution:
     def calculate(self, s: str) -> int:
         index = 0
 
+        def skip_spaces():
+            nonlocal index
+            index += 1
+            while index < len(s) and s[index].isspace():
+                index += 1
+
         def expr():
             nonlocal index
             result = factor()
             while index < len(s) and s[index] in "+-":
                 op = s[index]
-                index += 1
+                skip_spaces()
                 if op == "+":
                     result += factor()
                 else:
@@ -19,14 +25,15 @@ class Solution:
             nonlocal index
             result = 0
             if index < len(s) and s[index] == "(":
-                index += 1
+                skip_spaces()
                 result = expr()
-                index += 1  # ")"を読み飛ばす
+                skip_spaces()  # ")"を読み飛ばす
                 return result
             while index < len(s) and s[index].isdigit():
                 result = 10 * result + int(s[index])
-                index += 1
+                skip_spaces()
             return result
 
-        s = s.replace(" ", "")
+        if index < len(s) and s[0].isspace():
+            skip_spaces()
         return expr()
