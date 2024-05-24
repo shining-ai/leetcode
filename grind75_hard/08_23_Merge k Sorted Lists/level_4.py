d790cb6f99c28617860a91d6e823fce0acaadf0b
shining-ai <tderkkwii+social@gmail.com>
2024-05-24 15:45:01 +0000

    マージソートをイメージした解法を追加

--- grind75_hard/08_23_Merge k Sorted Lists/level_4.py
+++ grind75_hard/08_23_Merge k Sorted Lists/level_4.py
@@ -17,3 +17,36 @@ class Solution:
                     min_heap, (smallest_head.next.val, index, smallest_head.next)
                 )
         return sentinel.next
+
+
+# マージソートをイメージした解法
+# 先頭の2つのリストをマージしていき、最後の1つになるまで繰り返す
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def merge_two_lists(list_1, list_2):
+            sentinel = ListNode()
+            node = sentinel
+            while list_1 and list_2:
+                if list_1.val < list_2.val:
+                    node.next = list_1
+                    list_1 = list_1.next
+                else:
+                    node.next = list_2
+                    list_2 = list_2.next
+                node = node.next
+            if not list_1:
+                node.next = list_2
+            else:
+                node.next = list_1
+            return sentinel.next
+
+        list_queue = deque(lists)
+        if not list_queue:
+            return None
+        while 1:
+            list_1 = list_queue.popleft()
+            if not list_queue:
+                return list_1
+            list_2 = list_queue.popleft()
+            mearged_list = merge_two_lists(list_1, list_2)
+            list_queue.append(mearged_list)
