a79e259481e39270eee17e770fcf485d491c5f7d
shining-ai <tderkkwii+social@gmail.com>
2024-07-03 12:13:57 +0000

    解き方を整理

--- arai60/06-07_Stack/07_206_Reverse Linked List/level_1.py
+++ arai60/06-07_Stack/07_206_Reverse Linked List/level_1.py
@@ -1,33 +1,48 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# 実直な解法
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sentinel = ListNode()
+        node = head
+        prev_node = None
+        while node:
+            next_node = node.next
+            node.next = prev_node
+            sentinel.next = node
+            prev_node = node
+            node = next_node
+        return sentinel.next
 
 
-def reverse_list(head: ListNode) -> bool:
-    prev = None
+# 再帰を使った解法1
+# リストの接続は呼び出し先の関数にやってもらう
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse_list_helper(node):
+            if not node.next:
+                return node, node
+            reversed_head, reversed_tail = reverse_list_helper(node.next)
+            node.next = None
+            reversed_tail.next = node
+            return reversed_head, reversed_tail.next
 
-    while head != None:
-        next_node = head.next
-        head.next = prev
-        prev = head
-        head = next_node
-
-    return prev
+        if not head:
+            return None
+        head, tail = reverse_list_helper(head)
+        return head
 
 
-if __name__ == "__main__":
-    l = [3, 2, 0, -4]
-    pos = 1
+# 再帰を使った解法2
+# リストの接続をやってから次の関数に渡す
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse_list_helper(reversed, rest):
+            if not rest:
+                return reversed
+            rest_head = rest.next
+            reversed_tail = reversed
+            ans = rest
+            ans.next = reversed_tail
+            ans = reverse_list_helper(ans, rest_head)
+            return ans
 
-    dummy_head = ListNode(0)
-    curr = dummy_head
-    for i in l:
-        node = ListNode(i)
-        curr.next = node
-        curr = node
-
-    ans = reverse_list(dummy_head.next)
-    while ans != None:
-        print(ans.val)
-        ans = ans.next
+        return reverse_list_helper(None, head)
--- arai60/06-07_Stack/07_206_Reverse Linked List/level_2.py
+++ arai60/06-07_Stack/07_206_Reverse Linked List/level_2.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# 実直な解法
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
@@ -12,3 +8,35 @@ class Solution:
             prev = head
             head = next_node
         return prev
+
+
+# 再帰を使った解法1
+# リストの接続は呼び出し先の関数にやってもらう
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse_list_helper(node):
+            if not node:
+                return None
+            if not node.next:
+                return node
+            head = reverse_list_helper(node.next)
+            node.next.next = node  # reverse_listのtailはnode.next
+            node.next = None
+            return head
+
+        return reverse_list_helper(head)
+
+
+# 再帰を使った解法2
+# リストの接続をやってから次の関数に渡す
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse_list_helper(reversed, rest):
+            if not rest:
+                return reversed
+            remain = rest.next
+            rest.next = reversed
+            rest = reverse_list_helper(rest, remain)
+            return rest
+
+        return reverse_list_helper(None, head)
