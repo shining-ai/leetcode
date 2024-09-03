56aa07cb0a41f7647665512abb675686d36cf923
shining-ai <tderkkwii+social@gmail.com>
2024-09-03 16:35:59 +0000

    解き直し

---
+++ arai60/01-05_LinkedList/04_082_Remove Duplicates from Sorted List II/rework.py
@@ -0,0 +1,32 @@
+# fixは確定しているものと考える。
+# fix.nextにふさわしいかどうかを判定していく。
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sentinel = ListNode(0, head)
+        fix = sentinel
+        while head and head.next:
+            if head.val != head.next.val:
+                fix = fix.next
+                head = head.next
+                continue
+            while head.next and head.val == head.next.val:
+                head = head.next
+            head = head.next
+            fix.next = head
+        return sentinel.next
+
+
+# nodeを直接編集する
+# node.nextとnode.next.nextを比較することで、node.nextがふさわしいかどうかを判定していく。
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sentinel = ListNode(0, head)
+        node = sentinel
+        while node and node.next and node.next.next:
+            if node.next.val != node.next.next.val:
+                node = node.next
+                continue
+            val = node.next.val
+            while node.next and node.next.val == val:
+                node.next = node.next.next
+        return sentinel.next
