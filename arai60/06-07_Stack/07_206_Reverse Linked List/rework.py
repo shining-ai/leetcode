ff580d0a62d3e93a4f0ad9e38e940930b2085a68
shining-ai <tderkkwii+social@gmail.com>
2024-09-08 17:08:36 +0000

    解き直し

---
+++ arai60/06-07_Stack/07_206_Reverse Linked List/rework.py
@@ -0,0 +1,44 @@
+# 単純な解法
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        reversed_list = None
+        while head:
+            next_head = head.next
+            head.next = reversed_list
+            reversed_list = head
+            head = next_head
+        return reversed_list
+
+
+# 再帰
+# 先頭と末尾が返ってくれば、作業ができると考える
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        def helper(node):
+            if not node:
+                return None, None
+            if not node.next:
+                return node, node
+            head, tail = helper(node.next)
+            tail.next = node
+            tail = tail.next
+            tail.next = None
+            return head, tail
+
+        reverse_list, _ = helper(head)
+        return reverse_list
+
+
+# 再帰
+# ひっくり返ったものを返してもらう
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def helper(reverse_list, rest):
+            if not rest:
+                return reverse_list
+            rest_head = rest.next
+            rest.next = reverse_list
+            return helper(rest, rest_head)
+
+        return helper(None, head)
