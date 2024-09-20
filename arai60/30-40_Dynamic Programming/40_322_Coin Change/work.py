a0bf051c7187d9f3e2c7c777659ed43796eef927
shining-ai <tderkkwii+social@gmail.com>
2024-09-20 14:16:52 +0000

    解き直し

--- arai60/30-40_Dynamic Programming/40_322_Coin Change/work.py
+++ arai60/30-40_Dynamic Programming/40_322_Coin Change/work.py
@@ -16,3 +16,25 @@ class Solution:
                 remains_queue.append((next_remain, coin_num + 1))
                 seen.add(next_remain)
         return -1
+
+
+# 再帰で解く
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        @cache
+        def calc_min_coin(remain):
+            if remain == 0:
+                return 0
+            min_coin = math.inf
+            for coin in coins:
+                next_remain = remain - coin
+                if next_remain < 0:
+                    continue
+                coin_num = calc_min_coin(next_remain) + 1
+                min_coin = min(min_coin, coin_num)
+            return min_coin
+
+        coin_num = calc_min_coin(amount)
+        if coin_num == math.inf:
+            return -1
+        return coin_num
