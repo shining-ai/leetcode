e82f2855f1c97b0ace621040a4e8a538d638c52f
shining-ai <tderkkwii+social@gmail.com>
2024-10-23 13:05:06 +0000

    解き直し

--- arai60/30-40_Dynamic Programming/35_198_House Robber/rework.py
+++ arai60/30-40_Dynamic Programming/35_198_House Robber/rework.py
@@ -1,11 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 1:
-            return 0
-        max_amount_before_2 = 0
+        current_max = 0
         max_amount_before_1 = 0
+        max_amount_before_2 = 0
         for num in nums:
-            current_max = max(max_amount_before_2 + num, max_amount_before_1)
+            current_max = max(max_amount_before_1, max_amount_before_2 + num)
             max_amount_before_2 = max_amount_before_1
             max_amount_before_1 = current_max
         return current_max
